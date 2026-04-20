c5870e6c24e3005a5b74f125cf846720d83583a6
adolfo antequera <lacresta_20@hotmail.com>
2026-04-20 16:24:36 -0400

    fix: secret key

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -24,7 +24,8 @@ def get_secret(key, default=None):
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_secret('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY", get_secret("SECRET_KEY"))
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
