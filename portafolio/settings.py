f14bed024decbc540bbc0961a02e6af3aaca3977
adolfo antequera <lacresta_20@hotmail.com>
2026-04-20 16:45:59 -0400

    DB sqlite

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -5,6 +5,8 @@ from pathlib import Path
 import os
 import json
 
+import dj_database_url
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,12 +83,15 @@ WSGI_APPLICATION = 'portafolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=False
+    )
 }
+
 
 
 # Password validation
